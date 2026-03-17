3f7accb82649a2d0f33103d3b3659ad02b36de36
GeorgNewgalaxy <105148252+GeorgNewgalaxy@users.noreply.github.com>
2026-03-17 18:10:14 +0400

    Update recipe.lua

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -11,18 +11,22 @@ locomotive_mk2.result = nil
 locomotive_mk2.results = {{type = "item", name = "locomotive-mk2", amount = 1}}
 
 cargo_wagon_mk2.ingredients = {
+    {type = "item", name = "cargo-wagon", amount = 1},
     {type = "item", name = "low-density-structure", amount = 50},
     {type = "item", name = "iron-gear-wheel", amount = 100},
     {type = "item", name = "plastic-bar", amount = 50},
+    {type = "item", name = "steel", amount = 100},
     {type = "item", name = "processing-unit", amount = 20},
     {type = "fluid", name = "lubricant", amount = 100}
 }
 cargo_wagon_mk2.category = "crafting-with-fluid"
 
 locomotive_mk2.ingredients = {
+    {type = "item", name = "locomotive", amount = 1},
     {type = "item", name = "low-density-structure", amount = 75},
     {type = "item", name = "iron-gear-wheel", amount = 150},
     {type = "item", name = "plastic-bar", amount = 50},
+    {type = "item", name = "steel", amount = 100},
     {type = "item", name = "processing-unit", amount = 30},
     {type = "fluid", name = "lubricant", amount = 100}
 }
